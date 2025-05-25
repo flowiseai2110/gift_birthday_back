22c0c09b7bb2c1632ae60b492c4b0907019959ab
flowiseai2110 <flowiseai2110@gmail.com>
2025-05-25 08:00:19 -0500

    test 1

--- API.CORE.SUPABASE/Controllers/WeatherForecastController.cs
+++ API.CORE.SUPABASE/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WEB.API.ECOMMERCE.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
