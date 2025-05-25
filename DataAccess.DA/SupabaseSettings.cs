3c53c1ea7bf2b222a345163c2a560e34bcac31ae
flowiseai2110 <flowiseai2110@gmail.com>
2025-05-25 06:39:08 -0500

    primer commit

---
+++ DataAccess.DA/SupabaseSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DA
+{
+    public class SupabaseSettings
+    {
+        public string Url { get; set; } = string.Empty;
+        public string Key { get; set; } = string.Empty;
+        public string BucketName { get; set; } = string.Empty;
+        public string StorageUrl { get; set; } = string.Empty;
+        public string AuthUrl { get; set; } = string.Empty;
+        public string ProjectRef { get; set; } = string.Empty;
+    }
+}
---
+++ Negocio.BL/PersonaBL.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.DA.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.BL
+{
+    public class PersonaBL
+    {
+        public Task<List<Persona>> getListPersona() { 
+            return new SupabaseService().getListPersona();
+        }
+    }
+}
