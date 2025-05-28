e229193e39631071a8b1e6896cd9c82a9ce76e73
PRONABEC\diego.luna <diego.luna@pronabec.gob.pe>
2025-05-27 19:28:50 -0500

    scrapear

--- API.CORE.SUPABASE/Controllers/WeatherForecastController.cs
+++ API.CORE.SUPABASE/Controllers/WeatherForecastController.cs
@@ -1,9 +1,13 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Web.Api.Domain.Entities;
 using Web.Api.Interface.Persistence.GiftBirthDay;
 using Web.Api.Interface.UseCases.Servicios.Empresas;
 using Web.Api.UseCases.Services.GiftBirthDay;
 using Web.Api.UseCases.Services.GiftBirthDay.Queries;
+using HtmlAgilityPack;
+using API.CORE.SUPABASE;
+
 
 namespace WEB.API.ECOMMERCE.Controllers
 {
@@ -21,8 +25,19 @@ namespace WEB.API.ECOMMERCE.Controllers
         public async Task<IActionResult> GetPersonas()
         {
             var responde = await _mediator.Send(new GetUsuariosQuery());
-            //cmabio1
+            
             return Ok(responde);
         }
-    }
+
+        [HttpGet("web_externa")]
+        public async Task<IActionResult> GetWebExterna()
+        {
+            var responde = await new ScrapearWeb().ScrapeProductosAsync();
+            
+            return Ok(responde);
+        }
+
+        
+    } 
+ 
 }
---
+++ API.CORE.SUPABASE/ProductoWebScrapt.cs
@@ -0,0 +1,92 @@
+﻿using HtmlAgilityPack;
+
+namespace API.CORE.SUPABASE
+{
+    public class ProductoWebScrapt
+    {
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string imagen_url { get; set; }
+        public string marca { get; set; }
+        public string precio { get; set; }
+
+
+    }
+    public class ScrapearWebAlibaba
+    {
+
+        public async Task<List<ProductoWebScrapt>> ScrapeProductosAsync()
+        {
+            var productos = new List<ProductoWebScrapt>();
+            var url = "https://www.tailoy.com.pe/mundo-bebe/aseo-del-bebe/aseo-personal-para-bebes.html?p=2"; // Reemplaza con la URL real
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var nodos = doc.DocumentNode.SelectNodes("//li[contains(@class, 'product-item')]");
+
+            if (nodos != null)
+            {
+                foreach (var nodo in nodos)
+                {
+                    var nombre = nodo.SelectSingleNode(".//div[contains(@class, 'label')]")?.InnerText.Trim();
+                    //var marca = nodo.SelectSingleNode(".//div[contains(@class, 'brand-label')]")?.InnerText.Trim();
+                    var descripcion = nodo.SelectSingleNode(".//a[contains(@class, 'product-item-link')]")?.InnerText.Trim();
+                    var imagen = nodo.SelectSingleNode(".//img")?.GetAttributeValue("src", "");
+                    var price = nodo.SelectSingleNode(".//span[@class='price']")?.InnerText;
+
+                    productos.Add(new ProductoWebScrapt
+                    {
+                        nombre = nombre,
+                        descripcion = descripcion,
+                        //marca = marca,
+                        imagen_url = imagen,
+                        precio = price
+                    });
+                }
+            }
+
+            return productos;
+        }
+    }
+    public class ScrapearWeb {
+
+        public async Task<List<ProductoWebScrapt>> ScrapeProductosAsync()
+        {
+            var productos = new List<ProductoWebScrapt>();
+            var url = "https://www.tailoy.com.pe/mundo-bebe/aseo-del-bebe/aseo-personal-para-bebes.html?p=2"; // Reemplaza con la URL real
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var nodos = doc.DocumentNode.SelectNodes("//li[contains(@class, 'product-item')]");
+
+            if (nodos != null)
+            {
+                foreach (var nodo in nodos)
+                {
+                    var nombre = nodo.SelectSingleNode(".//div[contains(@class, 'label')]")?.InnerText.Trim();
+                    //var marca = nodo.SelectSingleNode(".//div[contains(@class, 'brand-label')]")?.InnerText.Trim();
+                    var descripcion = nodo.SelectSingleNode(".//a[contains(@class, 'product-item-link')]")?.InnerText.Trim();
+                    var imagen = nodo.SelectSingleNode(".//img")?.GetAttributeValue("src", ""); 
+                    var price = nodo.SelectSingleNode(".//span[@class='price']")?.InnerText;
+
+                    productos.Add(new ProductoWebScrapt
+                    {
+                        nombre = nombre,
+                        descripcion = descripcion,
+                        //marca = marca,
+                        imagen_url = imagen,
+                        precio = price
+                    });
+                }
+            }
+
+            return productos;
+        }
+    }
+}
